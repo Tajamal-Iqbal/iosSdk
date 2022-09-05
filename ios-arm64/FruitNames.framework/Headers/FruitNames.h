d611d277bce4982d796f8ae8271aeb16c054ad57
Rashid Saleem <87074582+Tajamal-Iqbal@users.noreply.github.com>
2022-09-05 18:09:18 +0500

    xcframe

---
+++ ios-arm64/FruitNames.framework/Headers/FruitNames.h
@@ -0,0 +1,18 @@
+//
+//  FruitNames.h
+//  FruitNames
+//
+//  Created by Toseef Ahmed on 05/09/2022.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for FruitNames.
+FOUNDATION_EXPORT double FruitNamesVersionNumber;
+
+//! Project version string for FruitNames.
+FOUNDATION_EXPORT const unsigned char FruitNamesVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FruitNames/PublicHeader.h>
+
+
