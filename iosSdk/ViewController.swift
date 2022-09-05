6d45643e27c034fa8016a08ceffc19de70a79504
Rashid Saleem <87074582+Tajamal-Iqbal@users.noreply.github.com>
2022-09-05 12:51:03 +0500

    iossdk

---
+++ iosSdk/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  iosSdk
+//
+//  Created by Toseef Ahmed on 05/09/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
